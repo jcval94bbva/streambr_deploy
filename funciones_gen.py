fd86ed10980786d9dc92a5ea25cb02726e54736a
jcval94bbva <149200470+jcval94bbva@users.noreply.github.com>
2023-10-31 11:20:34 -0600

    Update funciones_gen.py
    con por string

--- funciones_gen.py
+++ funciones_gen.py
@@ -20,8 +20,8 @@ def get_emojis():
 	return conceptos_con_emojis
 
 def assigne_emoj(string, emoj):
+	string = string.replace('Credito','Crédito').replace('Pagares','Pagarés').replace('Captacion','Captación').replace('Nomina','Nómina')
 	for con, em in emoj.items():
-		con = con.replace('Credito','Crédito').replace('Pagares','Pagarés').replace('Captacion','Captación').replace('Nomina','Nómina')
 		mostrar = em + con
 		string = string.replace(con,mostrar)
 	return string
