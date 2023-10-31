148d93e9a725199f34f5f41767a8000d359680b9
jcval94bbva <149200470+jcval94bbva@users.noreply.github.com>
2023-10-31 11:03:08 -0600

    Add files via upload
    crear funciones por separado

---
+++ funciones_gen.py
@@ -0,0 +1,20 @@
+
+def get_emojis():
+	conceptos_con_emojis = {
+      "Captación": "💰",
+      "Seguro de Vida": "👨‍👩‍👧‍👦🛡️",  # Representa una familia y un escudo
+      "Portabilidad": "📲",
+      "Seguro de Salud": "🏥🛡️",
+      "Crédito de Nómina": "💳🧾",
+      "Seguro de Auto": "🚗🛡️",
+      "Seguro de Hogar": "🏡🛡️",
+      "Fondos": "💰💼",
+      "Pagarés": "📝",
+      "Tarjeta de Crédito": "💳",
+      "Crédito Hipotecario": "🏠🏦",
+      "PPI": "💸",
+      "Crédito de Auto": "🚗💳",
+      "EFI": "💵",
+      "ILC": "📈💳"
+    }
+	return conceptos_con_emojis
